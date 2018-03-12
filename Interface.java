dc05e1bb8727fd8d92723a0c58c420368b5a7d40
Boychev <boychev@egb070000046.lancs.local>
2018-03-12 20:33:45 +0000

    after move

--- Interface.java
+++ Interface.java
@@ -1,11 +1,24 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Interface{
-	public Interface(){
-	
-	JFrame frame = new JFrame();
+public class Interface extends JLabel implements ActionListener {
+	JFrame frame = new JFrame(); // moved the objects out of constructor
 	JPanel panel = new JPanel();
+
+	ImageIcon i0 = new ImageIcon("bart0.jpg");	
+	ImageIcon i1 = new ImageIcon("bart1.jpg");
+	ImageIcon i2 = new ImageIcon("bart2.jpg");
+	ImageIcon i3 = new ImageIcon("bart3.jpg");
+	ImageIcon i4 = new ImageIcon("bart4.jpg");
+	ImageIcon i5 = new ImageIcon("bart5.jpg");
+	ImageIcon i6 = new ImageIcon("bart6.jpg");
+	ImageIcon i7 = new ImageIcon("bart7.jpg");
+	ImageIcon i8 = new ImageIcon("bart8.jpg");	
+	ImageIcon i9 = new ImageIcon("bart9.jpg");
+	ImageIcon i10 = new ImageIcon("bart10.jpg");
+	ImageIcon i11 = new ImageIcon("bart11.jpg");
+	
+	public Interface(){
 	
 	frame.setContentPane(panel);
 	frame.setSize(448,360);
@@ -14,30 +27,30 @@ public class Interface{
 	GridLayout layout = new GridLayout(3,4);
 	panel.setLayout(layout);
 
-	ImageIcon i0 = new ImageIcon("bart0.jpg");
-	JButton b0 = new JButton(i0);
-	ImageIcon i1 = new ImageIcon("bart1.jpg");
+	JButton b0 = new JButton(i0); // probably b0 is not for push???
+	
 	JButton b1 = new JButton(i1);
-	ImageIcon i2 = new ImageIcon("bart2.jpg");
+	b1.addActionListener(this); 
 	JButton b2 = new JButton(i2);
-	ImageIcon i3 = new ImageIcon("bart3.jpg");
+	b2.addActionListener(this); 
 	JButton b3 = new JButton(i3);
-	ImageIcon i4 = new ImageIcon("bart4.jpg");
-	JButton b4 = new JButton(i4);	
-	ImageIcon i5 = new ImageIcon("bart5.jpg");
+	b3.addActionListener(this); 
+	JButton b4 = new JButton(i4);
+	b4.addActionListener(this); 
 	JButton b5 = new JButton(i5);
-	ImageIcon i6 = new ImageIcon("bart6.jpg");
+	b5.addActionListener(this); 
 	JButton b6 = new JButton(i6);
-	ImageIcon i7 = new ImageIcon("bart7.jpg");
+	b6.addActionListener(this); 
 	JButton b7 = new JButton(i7);
-	ImageIcon i8 = new ImageIcon("bart8.jpg");
+	b7.addActionListener(this);
 	JButton b8 = new JButton(i8);
-	ImageIcon i9 = new ImageIcon("bart9.jpg");
+	b8.addActionListener(this); 
 	JButton b9 = new JButton(i9);
-	ImageIcon i10 = new ImageIcon("bart10.jpg");
+	b9.addActionListener(this); 
 	JButton b10 = new JButton(i10);	
-	ImageIcon i11 = new ImageIcon("bart11.jpg");
+	b10.addActionListener(this); 
 	JButton b11 = new JButton(i11);
+	b11.addActionListener(this); 
 
 	b0.setMargin(new Insets(0, 0, 0, 0));
 	b1.setMargin(new Insets(0, 0, 0, 0));
@@ -65,5 +78,10 @@ public class Interface{
 	panel.add(b10);
 	panel.add(b11);
 	frame.setVisible(true);
+	} // end of constructor
+	
+	public void actionPerformed(ActionEvent e){  
+	// swap the images here
+	
 	}
 }
