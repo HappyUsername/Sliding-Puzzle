f59026ed21a28d8bf0b08b50fd8b1499a5955c4d
Boychev <boychev@egb070000024.lancs.local>
2018-03-14 12:00:49 +0000

    After workshop

--- Interface.java
+++ Interface.java
@@ -4,7 +4,8 @@ import java.awt.event.*;
 import javax.net.*;
 import java.awt.Image;
 import javax.swing.ImageIcon;
-// https://codereview.stackexchange.com/questions/122150/logic-for-shuffling-sliding-puzzle
+import java.lang.Boolean;
+
 public class Interface extends JFrame implements ActionListener{
 	private JFrame frame = new JFrame("Puzzle"); // moved the objects out of constructor
 	private JPanel panel = new JPanel();
@@ -15,204 +16,79 @@ public class Interface extends JFrame implements ActionListener{
 	private JMenuItem Random = new JMenuItem("Random");
 	private JMenuItem About = new JMenuItem("About");
 
-
-	private ImageIcon i0 = new ImageIcon("bart0.jpg");	
-	ImageIcon i1 = new ImageIcon("bart1.jpg");
-	ImageIcon i2 = new ImageIcon("bart2.jpg");
-	ImageIcon i3 = new ImageIcon("bart3.jpg");
-	ImageIcon i4 = new ImageIcon("bart4.jpg");
-	ImageIcon i5 = new ImageIcon("bart5.jpg");
-	ImageIcon i6 = new ImageIcon("bart6.jpg");
-	ImageIcon i7 = new ImageIcon("bart7.jpg");
-	ImageIcon i8 = new ImageIcon("bart8.jpg");	
-	ImageIcon i9 = new ImageIcon("bart9.jpg");
-	ImageIcon i10 = new ImageIcon("bart10.jpg");
-	ImageIcon i11 = new ImageIcon("bart11.jpg");
+	private final int rows = 3;
+	private final int cols = 4;
+	private JButton tiles[][] = new JButton[rows][cols];
 	
-	JButton b0 = new JButton(i0); // probably b0 is not for push???
+	private int i,j;
 	
-	JButton b1 = new JButton(i1);
-	JButton b2 = new JButton(i2);
-	JButton b3 = new JButton(i3);
-	JButton b4 = new JButton(i4);
-	JButton b5 = new JButton(i5);
-	JButton b6 = new JButton(i6);
-	JButton b7 = new JButton(i7);
-	JButton b8 = new JButton(i8);
-	JButton b9 = new JButton(i9);
-	JButton b10 = new JButton(i10);	
-	JButton b11 = new JButton(i11);
 
 	public Interface(){
 
-	frame.setContentPane(panel);
-	frame.setSize(448,360);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	//frame.setJMenuBar(menuB);
-	//menuB.add(HighScore);
-	//menuB.add(Random);
-	//menuB.add(About);
+		frame.setContentPane(panel);
+		frame.setSize(448,360);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//frame.setJMenuBar(menuB);
+		//menuB.add(HighScore);
+		//menuB.add(Random);
+		//menuB.add(About);
 
-	GridLayout layout = new GridLayout(3,4);
-	panel.setLayout(layout);
- 
-
-	b0.setMargin(new Insets(0, 0, 0, 0));
-	b1.setMargin(new Insets(0, 0, 0, 0));
-	b2.setMargin(new Insets(0, 0, 0, 0));
-	b3.setMargin(new Insets(0, 0, 0, 0));
-	b4.setMargin(new Insets(0, 0, 0, 0));
-	b5.setMargin(new Insets(0, 0, 0, 0));
-	b6.setMargin(new Insets(0, 0, 0, 0));
-	b7.setMargin(new Insets(0, 0, 0, 0));
-	b8.setMargin(new Insets(0, 0, 0, 0));
-	b9.setMargin(new Insets(0, 0, 0, 0));
-	b10.setMargin(new Insets(0, 0, 0, 0));
-	b11.setMargin(new Insets(0, 0, 0, 0));
-
-	b1.addActionListener(this); 
-	b2.addActionListener(this);
-	b3.addActionListener(this); 
-	b4.addActionListener(this); 
-	b5.addActionListener(this); 
-	b6.addActionListener(this); 
-	b7.addActionListener(this);
-	b8.addActionListener(this); 
-	b9.addActionListener(this); 
-	b10.addActionListener(this); 
-	b11.addActionListener(this);
-
-	panel.add(b0);
-	panel.add(b1);
-	panel.add(b2);
-	panel.add(b3);
-	panel.add(b4);
-	panel.add(b5);
-	panel.add(b6);
-	panel.add(b7);
-	panel.add(b8);
-	panel.add(b9);
-	panel.add(b10);
-	panel.add(b11);
-	frame.setVisible(true);
+		GridLayout layout = new GridLayout(3,4);
+		panel.setLayout(layout);
+	 	
+		
+		initTiles();
+		drawPanel();
+	
+		frame.setVisible(true);
 	} // end of constructor
 	
-	public void actionPerformed(ActionEvent e){  
-	// swap the images here
+	public void initTiles(){
 		int counter = 0;
-
-		if (e.getSource() == b1  && 
-			(b0.getIcon() == i0 ||
-			b2.getIcon() == i0 ||
-			b5.getIcon() == i0 )){
-			if(b0.getIcon() == i0){
-				Image img = b1.getIcon();
-				ImageIcon iTemp = new ImageIcon();
-				iTemp = b1.getIcon();
-
-				b1.setIcon(i0);
-				b0.setIcon(iTemp);
-			}
-			else if(b2.getIcon() == i0){
-				b2.setIcon(i1);
-				b1.setIcon(i0);
-			}
-			else{
-				b5.setIcon(i1);
-				b1.setIcon(i0);
+		for (int i=0; i<rows; i++) {
+			for (int j=0; j<cols; j++) {
+				ImageIcon tileIcon = new ImageIcon("bart" + counter + ".jpg");
+				JButton tile = new JButton(tileIcon);
+				tile.setMargin(new Insets(0, 0, 0, 0));
+				tile.addActionListener(this);
+				tiles[i][j] = tile;
+				counter++;
 			}
 		}
-		if (e.getSource() == b2  && 
-			(b1.getIcon() == i0 ||
-			b3.getIcon() == i0 ||
-			b6.getIcon() == i0 ))
-		{
+	}
+	
+	ImageIcon iTemp = new ImageIcon(tiles[0][0].getIcon().toString());
 
-			//ImageIcon iTemp = new ImageIcon();
-			//iTemp = i2;
-			b2.setIcon(i0);
-			b0.setIcon(i2);
-		}
-		if (e.getSource() == b3 &&
-			(b2.getIcon() == i0 ||
-			b7.getIcon() == i0))
-		{
-			b3.setIcon(i0);
-			b0.setIcon(i3);
-		}
-		if (e.getSource() == b4 &&
-			(b0.getIcon() == i0 ||
-			b5.getIcon() == i0 ||
-			b8.getIcon() == i0 ))
-		{
-			b4.setIcon(i0);
-			b0.setIcon(i4);
-		}
-		if (e.getSource() == b5 &&
-			(b1.getIcon() == i0 ||
-			b4.getIcon() == i0 ||
-			b6.getIcon() == i0 ||
-			b9.getIcon()==i0))
-		{
-			b5.setIcon(i0);
-			b0.setIcon(i5);
-		}
-		if (e.getSource() == b6 && 
-			(b2.getIcon() == i0 ||
-			b5.getIcon() == i0 ||
-			b7.getIcon() == i0 ||
-			b10.getIcon()==i0))
-		{
-			b6.setIcon(i0);
-			b0.setIcon(i6);
-		}
-		if (e.getSource() == b7 && 
-			(b3.getIcon() == i0 ||
-			b6.getIcon() == i0 ||
-			b11.getIcon() == i0 ))
-		{
-			b7.setIcon(i0);
-			b0.setIcon(i7);
-		}
-		if (e.getSource() == b8 &&
-			(b4.getIcon() == i0 ||
-			b9.getIcon() == i0 ))
-		{
-			b8.setIcon(i0);
-			b0.setIcon(i8);
-		}
-		if (e.getSource() == b9 &&
-			(b5.getIcon() == i0 ||
-			b10.getIcon() == i0 ||
-			b8.getIcon() == i0 ))
-		{
-			b9.setIcon(i0);
-			b0.setIcon(i9);
-		}
-		if (e.getSource() == b10 &&
-			(b9.getIcon() == i0 ||
-			b6.getIcon() == i0 ||
-			b11.getIcon() == i0 ))
-		{
-			b10.setIcon(i0);
-			b0.setIcon(i10);
-		}
-		if (e.getSource() == b11 && 
-			(b7.getIcon() == i0 ||
-			b10.getIcon() == i0 ))
-		{	
-
-			if (b10.getIcon() == i0){
-					b10.setIcon(i11);
-					b11.setIcon(i0);
+	public void drawPanel(){
+		for (int i=0; i<rows; i++) {
+			for (int j=0; j<cols; j++) {
+				panel.add(tiles[i][j]);
 			}
-			else{
-					b7.setIcon(i11);
-					b11.setIcon(i0);
-			}
+		}
+		panel.repaint();
+	}
+	
+	public boolean distance(int i, int j) {
 
-		} // 
+		if(tiles[i-1][j].getIcon() == i0 ||
+		   tiles[i+1][j].getIcon() == i0 ||
+		   tiles[i][j-1].getIcon() == i0 || 
+		   tiles[i][j-1].getIcon() == i0 )return true;
 
+		else return false;
+	}
+
+	public void actionPerformed(ActionEvent e){  
+
+		int count = 0;
+	if (e.getSource() == tiles[0][1]){
+		if (distance(0,1) == true){
+			//System.out.println("FUCKEN WORKING");
+		
+		}
+	
+		
+	}	
 
 	 }// end of ActionPerformed
 
